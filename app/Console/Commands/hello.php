522f242437f4d132b69e79cca5c0ebddad4be32c
yamarico <cssu19093@g.nihon-u.ac.jp>
2022-12-21 21:08:18 +0900

    hello

---
+++ app/Console/Commands/hello.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class hello extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'execute:hello';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        echo 'Hello';
+        return Command::SUCCESS;
+    }
+}
