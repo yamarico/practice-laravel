3c350dd1dc22fbf77765b48dd1cf03a41373f40b
yamarico <cssu19093@g.nihon-u.ac.jp>
2022-12-21 21:49:44 +0900

    database-SQLite

--- app/Console/Commands/hello.php
+++ app/Console/Commands/hello.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class hello extends Command
 {
@@ -28,6 +29,20 @@ class hello extends Command
     public function handle()
     {
         echo 'Hello';
+
+        DB::table('users')->insert(
+            [
+                'name' => '山田たろう',
+                'email' => 'aaa3@gmail.com',
+                'email_verified_at' => '2022-12-21 21:22:00',
+                'password' => '123456',
+                'remember_token' => 'aaaaaaa',
+            ]
+        );
+
+        $list = DB::table('users')->get();
+        var_export($list->all());
+        
         return Command::SUCCESS;
     }
 }
